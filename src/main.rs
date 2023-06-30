0d48652258ce6a13e66cbacfa9eb777f84e25650
Jason Washburn <jason.washburn@gmail.com>
2023-06-29 20:35:19 -0500

    feat: :tada: initial working commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Jason Washburn")
+        .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input Text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+    let text = matches.values_of_lossy("text").unwrap();
+
+    print!("{}{}", text.join(" "), if matches.is_present("omit_newline") {""} else {"\n"});
+    
+}
